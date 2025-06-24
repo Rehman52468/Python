bc8546b9ac01bb1d8703fb86df1fedffe3495438
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-06-24 17:07:44 +0500

    add 4 rules for variables

---
+++ Chapter2/03_Rules_Variables.py
@@ -0,0 +1,11 @@
+a=90 # a is a correct variable name
+
+Rahman=6767 #Rahman is a correct Variable name
+
+_waqas=675787 #_waqas is a correct variable name
+
+@Rahman=7  # @rahman is not a correct variable name
+
+786Waqas=786 # 786 is not a correct variable name
+
+Waqas786=775 #Waqas786 is a correct variable name
