43d0d379d22841b742275646cd683205c673e657
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-06-24 17:13:15 +0500

    add 4 Rules for Variables

--- Chapter2/03_Rules_Variables.py
+++ Chapter2/03_Rules_Variables.py
@@ -4,8 +4,10 @@ Rahman=6767 #Rahman is a correct Variable name
 
 _waqas=675787 #_waqas is a correct variable name
 
-@Rahman=7  # @rahman is not a correct variable name
+#@Rahman=7  # @rahman is not a correct variable name
 
-786Waqas=786 # 786 is not a correct variable name
+#786Waqas=786 # 786 is not a correct variable name
 
 Waqas786=775 #Waqas786 is a correct variable name
+
+#Waqas@786=5433245 # Waqas@786 is not a correct variable name 
