ef2bc7714464f065f0ce14f526cc1da6f11f7da6
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-06-24 15:31:47 +0500

    added os module

--- Chapter1-Ps/Problem4.py
+++ Chapter1-Ps/Problem4.py
@@ -1,9 +1,7 @@
 import os
 
-# Specify the directory path
 directory = "/Riot Games/Riot Client"
 
-# Print the contents of the directory
 print(f"Contents of directory '{directory}':")
 for item in os.listdir(directory):
     print(item)
