ea7fb85f277cf87fbd4d4b2ae43ba2afade8fb73
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-02 12:44:17 +0500

    How Input Function works

---
+++ Chapter2/06_InputFunction.py
@@ -0,0 +1,20 @@
+# this program give you the output in strings mean if you want to add 1+2 this program will give u the output not 3 it will give you 12. If you want the output 3 
+# you have to use data type function int float bool none str.
+
+
+a = input("Enter Number 1:")
+b = input("Enter Number 2:")
+print("Number 1 is :",a)
+print("Number 2 is :",b)
+print("Sum is",a+b)
+
+
+X = int(input("Enter Number 5:"))
+Z = int(input("Enter Number 10:"))
+print("Number 5 is:", X)
+print( "Number 10 is:",Z)
+print("Sum is:",X+Z)
+
+a = float(input("Enter Number 1:"))
+T = type(a)
+print(T)
