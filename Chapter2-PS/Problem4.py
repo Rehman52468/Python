b43bbf9595d70a9bb7affe2ebffb9e23840ca49c
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-02 14:10:30 +0500

    Comparison Operator

---
+++ Chapter2-PS/Problem4.py
@@ -0,0 +1,22 @@
+# In this program we did not used the truth table of "and or not"
+A = 34
+B = 80
+print(A <= B)
+
+# In this program i will use truth table of "and".
+
+X = 50
+Z = 15
+print(X >= Z and X <= Z )
+
+# In this program i will use truth table of "or".
+
+a = 90
+b = 75
+print(a <= b or a >=b )
+
+# In this program i will use truth table of "not".
+
+R = 57
+X = 75
+print(not (R >= X) )
