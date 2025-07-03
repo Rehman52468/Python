31f5261a5b0b64bac3d3fb58c6ecdd77c5d2dcf5
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-03 17:24:24 +0500

    Types of Strings

---
+++ Chapter3/01_Strings.py
@@ -0,0 +1,5 @@
+Rehman = 'Rahman Afridi'        # This is single quoted string.
+
+Rehman = "Rahman Afridi"        # This is double quoted string.
+
+Rehman = '''Rahman Afridi'''    # This is triple quoted string.
