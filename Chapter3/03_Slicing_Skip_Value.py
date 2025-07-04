5a70e5cfcd6245d4dc728fb5a1c57c91834b919e
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-04 16:40:03 +0500

    skip values

---
+++ Chapter3/03_Slicing_Skip_Value.py
@@ -0,0 +1,20 @@
+R = "RahmanUllahAfridi"
+
+skip = R[2:15:4]
+
+print(skip)
+
+Name = "FarhanUllahAfridi"
+
+skiping = Name[6:17:5]
+
+print(skiping)
+
+
+# Advance Slicing techniques
+
+Name = "RahmanUllah"
+
+print(Name[4:])        #   Name[4:] is same as Name[4:10]
+
+print(Name[:6])        #   Name[:6] is same as Name[0:6]
