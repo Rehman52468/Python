49cf689a402020bef043eed73912d6660fc5e862
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-04 18:21:36 +0500

    Stringg Functions

---
+++ Chapter3/04_String_Functions.py
@@ -0,0 +1,59 @@
+Name = "Rahman, ullah, 1234567"
+
+Names = "12345abz"
+
+# Basic String Functions.
+
+print(len(Name))
+
+print(type(Name))
+
+print(str("Rahman ullah"))
+
+
+# String Modification Functions.
+
+print(Name.capitalize())
+
+print(Name.lower())
+
+print(Name.upper())
+
+print(Name.title())
+
+print(Name.strip())
+
+print(Name.replace("ullah","Ullah"))
+
+
+# Search & Checking.
+
+print(Name.endswith("lah"))
+
+print(Name.startswith("Rah"))
+
+print(Name.index("u"))
+
+print(Name.find("m"))
+
+print(Name in("Rahmanullah"))
+
+# String Content Checking.
+
+print(Name.isalpha())
+
+print(Name.isdigit())
+
+print(Name.isalnum())
+
+print(Name.isspace())
+
+# Joining and Splitting.
+
+print(Name.split())
+
+# print(Name.join(["Rahman ullah 1234567"]))
+
+
+
+
