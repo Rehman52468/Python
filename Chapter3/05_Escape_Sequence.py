80c3eed023c65e3bec4bb4cb32054a23107c6f91
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-08 16:25:05 +0500

    Escape Sequence Characters

---
+++ Chapter3/05_Escape_Sequence.py
@@ -0,0 +1,16 @@
+# Escape Sequences Characters
+                              # Escape Sequence Characters are used in strings. These are \n \t \' \" \\
+
+# \n is used to create a new line in the string
+#\t is used to create a tab space in the string
+#\' this character is used to put one or many words in single line quote 
+#\\ is used to create a backslash \.
+
+S = "Rahman Ullah \nloves the \\badminton \n\"sport\""
+
+print(S)
+  
+
+A = "Rahman Ullah \nneed a laptop \nbut \the cannot afford the \"laptop\" \n\\byhimself"
+
+print(A)
