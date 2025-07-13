3a50f05206c4cc2d0fa697d6e0c3cd5a824ebd03
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-13 16:56:37 +0500

    2 types of Indexing

---
+++ Chapter4/02_List _Indexing.py
@@ -0,0 +1,19 @@
+# Normal indexing Method.
+
+List1 = ["Farhan", "Asim", "Rahman", "Tariq" ,"Nasir", 7, 15, 10 ,55.7 ,100 ,True]
+
+List2 =List1 [0:6]
+
+print(List2)
+
+# Tricky indexing Method
+
+List1 = ["Farhan", "Asim", "Rahman", "Tariq" ,"Nasir", 7, 15, 10 ,55.7 ,100 ,True]
+
+List3 = List1[6:]
+
+List4 = List1[:7]
+
+print(List3)
+
+print(List4)
