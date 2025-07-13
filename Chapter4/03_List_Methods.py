16393a0448eddede38764fb3a3a8b3ab1247706e
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-13 18:21:05 +0500

    List Methods Functions

---
+++ Chapter4/03_List_Methods.py
@@ -0,0 +1,48 @@
+# These are some List Methods.
+
+List = [5 ,3 ,4, 1 ,99 ,95 ,150 ,125 ,285 ,2]
+
+List.sort()     # This Function put the data type in order. ye ek tarteeb day deta hai list ko.
+
+print(List)
+
+List.reverse()   # This Function reverse the data type if the lis is [12345] it will reverse the list [54321] yara ye ulta kar deta hai lis ko 
+                # sab say bari value pahlay phir us say chottey issi tarah. 
+
+print(List)
+
+List.append(500) # 
+
+print(List)
+
+List.insert(2, 200)
+
+print(List)
+
+List.pop(3)
+
+print(List)
+
+L1 = List.pop(3)
+
+print(L1)
+
+List.remove(99)
+
+print(List)
+
+List.clear()
+
+print(List)
+
+List.index(3)
+
+print(List)
+
+List.count(2)
+
+print(List)
+
+List.sort(reverse=True)
+
+print(List)
