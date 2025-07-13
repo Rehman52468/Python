a408818821a46a83bf482ae94f996d4e84c181b4
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-13 16:41:36 +0500

    what is list

---
+++ Chapter4/01_list.py
@@ -0,0 +1,7 @@
+# List.
+        # A list is a container that can store different type of data type. A list can store the integer boolean floating point number strings (data types).
+
+
+List = ["Farhan", "Asim", "Rahman", "Tariq" ,"Nasir", 7, 15, 10 ,55.7 ,100]
+
+print(List)
