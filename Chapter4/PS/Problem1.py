92c355a2ccc9c1869afe520405fa7798c655355d
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-14 17:47:07 +0500

    Storing Fruits Problem

---
+++ Chapter4/PS/Problem1.py
@@ -0,0 +1,23 @@
+List = []
+
+A1 = input("Enter fruit 1:")
+List.append(A1)
+A2 = input("Enter fruit 2:")
+List.append(A2)
+A3 = input("Enter fruit 3:")
+List.append(A3)
+A4 = input("Enter fruit 4:")
+List.append(A4)
+A5 = input("Enter fruit 5:")
+List.append(A5)
+A6 = input("Enter fruit 6:")
+List.append(A6)
+A7 = input("Enter fruit 7:")
+List.append(A7)
+
+print(List)
+
+
+
+
+
