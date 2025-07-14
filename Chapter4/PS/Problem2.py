aef9062ad37fa789540e570cb0be95cc1e003fda
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-14 17:59:45 +0500

    Solving 6Students marks Problem2

---
+++ Chapter4/PS/Problem2.py
@@ -0,0 +1,25 @@
+List = []
+
+Student1 = int(input("Enter Student No1:"))
+List.append(Student1)
+
+
+Student2 = int(input("Enter Student No2:"))
+List.append(Student2)
+
+Student3 = int(input("Enter Student No3:"))
+List.append(Student3)
+
+Student4 = int(input("Enter Student No4:"))
+List.append(Student4)
+
+Student5 = int(input("Enter Student No5:"))
+List.append(Student5)
+
+Student6 = int(input("Enter Student No6:"))
+List.append(Student6)
+
+List.sort()
+
+print(List)
+
