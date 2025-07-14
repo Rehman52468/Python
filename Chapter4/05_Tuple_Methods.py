45feb6c1d651b8dc504ac41e9ef72baccfb059f2
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-14 17:16:15 +0500

    Tuple Methods

---
+++ Chapter4/05_Tuple_Methods.py
@@ -0,0 +1,36 @@
+# Methods of Tuples.
+
+Tuple = (50, 100, 45, 10, "Rahman", "Farhan", "Zia", 500, 100)
+
+# Common Tuple Methods.
+
+Count = Tuple.count(100)         # This function counts the data type that the data types are repeated how many times.
+
+print(Count)
+
+Slicing = Tuple.index("Zia")     # This Function is used for slicing the Tuple.
+
+print(Slicing)
+
+# Other Tuple Tricks.
+
+Length = len(Tuple)              # This Function is used to see the length of the tuple it tells you that the tuple have 6 words length or 7 words length
+
+print(Length)
+
+In = "Zia" in Tuple              # This Function is used to see that if the data type or word or integer is in the tuple or not .This Function gives you 
+
+                                 # the output in boolean data type
+
+print(In)
+
+list = [1 ,1 , 2]
+
+converted = tuple(list)           # This Function is ussed to convert the List into Tuple. 
+
+print(converted)
+
+
+
+
+
