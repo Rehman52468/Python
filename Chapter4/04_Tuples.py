3a55d139ff69268dec9326e674c00cce60500137
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-14 15:00:12 +0500

    About Tuples

---
+++ Chapter4/04_Tuples.py
@@ -0,0 +1,14 @@
+# Tuples.
+        #  Tuples are immutable like Strings no one can add or remove anything in the tuples.
+
+Tuple = (10, 15, "Farhan", "Nasir", "Tariq")
+
+print(Tuple)
+
+Type = type(Tuple)
+
+print(Type)
+
+Length = len(Tuple)
+
+print(Length)
