83d573cb4a339af37ff8ce3f67ed85ed44b3cb60
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-26 14:25:06 +0500

    Howw to add Students Marks

---
+++ Chapter4/PS/Problem02.py
@@ -0,0 +1,50 @@
+List  = []
+
+S1 = input("Student no 1 :",)
+List.append(S1)
+
+S2 = input("Student no 2 :",)
+List.append(S2)
+
+S3 = input("Student no 3 :",)
+List.append(S3)
+
+S4 = input("Student no 4 :",)
+List.append(S4)
+
+S5 = input("Student no 5 :",)
+List.append(S5)
+
+S6 = input("Student no 6 :",)
+List.append(S6)
+
+List.sort()
+
+print(List)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
