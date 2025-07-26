3b8be0f9df80b5260890ec1770c8ea7d4f6b782b
Rehman52468 <RehmanUllahwc05@gmail.com>
2025-07-26 14:17:58 +0500

    How to put Things in List

---
+++ Chapter4/PS/Problem01.py
@@ -0,0 +1,37 @@
+List = []
+
+Fruit1 = input("Enter Fruit No1:",)
+List.append(Fruit1)
+
+Fruit2 = input("Enter Fruit No2:",)
+List.append(Fruit2)
+
+Fruit3 = input("Enter Fruit No3:",)
+List.append(Fruit3)
+
+Fruit4 = input("Enter Fruit No4:",)
+List.append(Fruit4)
+
+Fruit5 = input("Enter Fruit No5:",)
+List.append(Fruit5)
+
+Fruit6 = input("Enter Fruit No6:",)
+List.append(Fruit6)
+
+Fruit7 = input("Enter Fruit No7:",)
+List.append(Fruit7)
+
+print(List)
+
+
+
+
+
+
+
+
+
+
+
+
+
